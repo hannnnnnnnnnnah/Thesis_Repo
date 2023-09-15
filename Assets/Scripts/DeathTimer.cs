6fef86b3609c84c125414812ce61b0c72965a436
hannnnnnnnnnnah <hkey@mica.edu>
2023-09-15 10:17:34 -0400

    PLEASE

---
+++ Assets/Scripts/DeathTimer.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Rendering;
+
+public class DeathTimer : MonoBehaviour
+{
+    public float deathTime = 11;
+    public bool playVisuals = false;
+
+    [SerializeField] AudioSource breathing;
+    [SerializeField] AudioSource heartbeat;
+    [SerializeField] AudioSource metro;
+    [SerializeField] Volume vol;
+    [SerializeField] Animator animator;
+
+    private void Update()
+    {
+        if(deathTime == 0)
+        {
+            SceneManager.LoadScene("LightDeath_Test");
+            RespawnManager.instance.deathCount++;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (deathTime < 11 && vol.weight < 1 && playVisuals == true)
+            vol.weight += 0.002f;
+        if(playVisuals == false && vol.weight > 0)
+            vol.weight -= 0.01f;
+    }
+
+    public IEnumerator DeathTime()
+    {
+        while(deathTime > 0)
+        {
+            DeathAudioTrigger();
+            yield return new WaitForSeconds(1f);
+            deathTime--;
+            Debug.Log(deathTime);
+        }    
+    }
+
+    public void DeathAudioTrigger() 
+    {
+
+        switch (deathTime)
+        {
+            case 11:
+                playVisuals = true;
+                animator.SetBool("DV", true);
+
+                breathing.mute = false;
+                heartbeat.mute = false;
+                metro.mute = false;
+
+                if (!breathing.isPlaying)
+                {
+                    breathing.Play();
+                    Debug.Log("breathing");
+                }
+                break;
+
+            case 10:
+                if (!metro.isPlaying)
+                {
+                    metro.Play();
+                    Debug.Log("dark entered");
+                }
+                break;
+
+            case 6:
+                if (!heartbeat.isPlaying)
+                {
+                    heartbeat.Play();
+                    Debug.Log("heartbeat");
+                }
+                break;
+        }
+
+    }
+
+    public IEnumerator DeathEffectFade()
+    {
+        breathing.mute = true;
+        heartbeat.mute = true;
+        metro.mute = true;
+        yield return new WaitForSeconds(1f);
+        breathing.Stop();
+        heartbeat.Stop();
+        metro.Stop();
+    }
+
+    public void DeathEffectsCancel()
+    {
+        StartCoroutine(DeathEffectFade());
+        animator.SetBool("DV", false);
+        playVisuals = false;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController characterController;
+
+    [SerializeField] float sprintSpeed, walkSpeed, gravity;
+    [SerializeField] int minAngle, maxAngle;
+
+    public int sensitivity = 140;
+
+    private float speed, stepRate, stepCoolDown, stepRateSet;
+    private Vector3 camRotation, moveDirection;
+    private Camera mainCamera;
+
+    [SerializeField] AudioSource audioFoot;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        mainCamera = Camera.main;
+        speed = walkSpeed;
+    }
+
+    void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    private void Move()
+    {
+        stepRate = 0.4f;
+
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+
+        if (characterController.isGrounded)
+        {
+            moveDirection = new Vector3(horizontalMove, 0, verticalMove);
+            moveDirection = transform.TransformDirection(moveDirection);
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                speed = sprintSpeed;
+                stepRateSet = 0.2f;
+
+            }
+            else
+            {
+                speed = walkSpeed;
+                stepRateSet = stepRate;
+            }
+
+            //step audio
+
+            stepCoolDown -= Time.deltaTime;
+
+            if (characterController.velocity.magnitude > 0 && characterController.velocity.y == 0 && stepCoolDown < 0f)
+            {
+                audioFoot.pitch = 1f + Random.Range(-0.1f, 0.1f);
+                audioFoot.Play();
+                stepCoolDown = stepRateSet;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        if (characterController.enabled)
+        {
+            characterController.Move(moveDirection * speed * Time.deltaTime);
+        }
+    }
+
+    private void Rotate()
+    {
+        transform.Rotate(Vector3.up * sensitivity * Time.deltaTime * (Input.GetAxis("Mouse X")));
+
+        camRotation.x -= Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        camRotation.x = Mathf.Clamp(camRotation.x, minAngle, maxAngle);
+
+        mainCamera.transform.localEulerAngles = camRotation;
+    }
+}
