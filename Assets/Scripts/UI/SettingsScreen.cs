bf9e96a1c4f048788ac5e93d8b34352a7221e0b0
artanne <145148831+artanne@users.noreply.github.com>
2024-04-12 18:47:40 -0400

    subtit button

--- Assets/Scripts/UI/SettingsScreen.cs
+++ Assets/Scripts/UI/SettingsScreen.cs
@@ -1,11 +1,23 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class SettingsScreen : MonoBehaviour
 {
     [SerializeField] GameObject settingsScreen;
     [SerializeField] GameObject pauseScreen;
     [SerializeField] TextMeshProUGUI SubtitleText;
+    [SerializeField] Image SubtitlesButtonImage;
+    public static Sprite onButton, offButton;
+    public bool subtitles;
+
+    public void Start()
+    {
+        onButton = Resources.Load<Sprite>("on_button");
+        offButton = Resources.Load<Sprite>("off_button");
+        subtitles = true;
+    }
     public void BackToPause()
     {
         pauseScreen.SetActive(true);
@@ -14,9 +26,19 @@ public class SettingsScreen : MonoBehaviour
 
     public void ChangeSubtitles()
     {
-        if(SubtitleText.alpha > 0f)
+        if (SubtitleText.alpha > 0f && subtitles == true)
+        {
             SubtitleText.alpha = 0f;
+            SubtitlesButtonImage.sprite = offButton;
+            subtitles = false;
+            EventSystem.current.SetSelectedGameObject(null);
+        }
         else
+        {
             SubtitleText.alpha = 255f;
+            SubtitlesButtonImage.sprite = onButton;
+            subtitles = true;
+            EventSystem.current.SetSelectedGameObject(null);
+        }
     }
 }
